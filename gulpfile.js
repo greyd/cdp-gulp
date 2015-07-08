9fc85430208a8e666090dfc44fafbafa386ca494
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-07-08 05:29:12 +0300

    Add script-build task

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var concat = require('gulp-concat');
 var imagemin = require('gulp-imagemin');
 var spritesmith = require('gulp.spritesmith');
 var del = require('del');
+var htmlreplace = require('gulp-html-replace');
+var uglify = require('gulp-uglify');
+var mainBowerFiles = require('main-bower-files');
+var filter = require('gulp-filter');
 
 var conf = {
     less: 'src/less/*.less',
@@ -27,8 +31,9 @@ var conf = {
     },
     release: {
         folder: 'release',
-        css: 'release/css',
-        images: 'release/images'
+        css: 'release/build/css',
+        js: 'release/build/js',
+        images: 'release/build/images'
     }
 
 };
@@ -71,12 +76,27 @@ gulp.task('sprite', function () {
     return spriteData.pipe(gulp.dest('src/'));
 });
 gulp.task('prepare', ['style', 'images']);
-gulp.task('build', ['style-build', 'images-build']);
+gulp.task('build', ['style-build', 'images-build', 'html-build', 'script-build']);
 gulp.task('watch', ['prepare'], function () {
     return gulp.watch(conf.less, ['style']);
 });
 gulp.task('clean', function (cb) {
     del([conf.release.folder, conf.build.folder], cb);
+});
+gulp.task('html-build', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlreplace({
+            'css': '../build/css/cdp.css',
+            'js': '../build/js/cdp.js'
+        }))
+        .pipe(gulp.dest('release/tpl'));
+});
+gulp.task('script-build', ['bower'], function () {
+    return gulp.src(mainBowerFiles({includeDev: true}))
+        .pipe(filter('*.js'))
+        .pipe(concat('cdp.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(conf.release.js));
 });
 
 function errorHandler(err){
